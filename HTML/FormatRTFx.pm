4971b372da2e53d0323455a239e5e6cc52d4423d
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-05-17 11:55:18 +0200

    add PAGE_WIDTH setting; fixed typo with trgaph

--- HTML/FormatRTFx.pm
+++ HTML/FormatRTFx.pm
@@ -5,7 +5,9 @@ use warnings;
 use base 'HTML::FormatRTF';
 use Data::Dumper;
 
-# mapping for fonts: use {\f$font_map{lc(font_name)} text} to add a font. 
+my $PAGE_PX_WIDTH = 800; # assume a base page width of 800px (for wkhtml2pdf)
+
+# mapping for fonts: use {\f$font_map{lc(font_name)} text} to add a font.
 my %font_map = (
 	"courier new" => 3,
 	"lucida console" => 4,
@@ -15,7 +17,7 @@ my %font_map = (
 	"georgia" => 8,
 	"tahoma" => 9,
 	"lucida sans" => 10,
-	"verdana" => 11	
+	"verdana" => 11
 );
 
 # ------------------------------------------------------------------------
@@ -28,8 +30,8 @@ sub begin {
 
     $self->collect( $self->doc_init, $self->font_table, $self->stylesheet, $self->color_table, $self->doc_info,
     	$self->doc_format,
-        $self->doc_really_start, "\n" )
-        unless $self->{'no_prolog'};
+	$self->doc_really_start, "\n" )
+	unless $self->{'no_prolog'};
 
     $self->{'Para'}       = '';
     $self->{'quotelevel'} = 0;
@@ -52,7 +54,7 @@ sub doc_format{
 	my $self = shift;
 	$self->out(\'\paperw'.$self->{'paperw'});
 	$self->out(\'\paperh'.$self->{'paperh'});
-	$self->out(\'\margl'.$self->{'margl'});		
+	$self->out(\'\margl'.$self->{'margl'});
 	$self->out(\'\margr'.$self->{'margr'});
 	$self->out(\'\margt'.$self->{'margt'});
 	$self->out(\'\margb'.$self->{'margb'});
@@ -67,30 +69,45 @@ sub table_start{
 
 sub tr_start{
 	my $self = shift;
-	
+
 	my $node = shift;
 	my $tag = $node->tag;
-	
+
 	my @cells = grep { $_->tag =~ /td|th/   } $node->descendants();
 	my $cell_count = @cells;
 	my $twips_width = $self->{'paperw'} - $self->{'margr'} - $self->{'margl'}; # page width - left and right margin.
 	my $cell_padding = 10;
-	$twips_width = $twips_width - 20 - $cell_count * $cell_padding ; # subtract 20twips left border, minus 10 for each cellpadding.  
-	
-	$self->out( \"{\\pard\\trowd\\tgraph10\\trleft10");
-	
+	$twips_width = $twips_width - 20 - $cell_count * $cell_padding ; # subtract 20twips left border, minus 10 for each cellpadding.
+
+	# debugging.
+	# pard: reset paragraph style to default
+	# trowd: begin table row
+	# trgaphN: "Half the space between the cells of a table row in twips."
+	# trleftN: "Position of the leftmost edge of the table with respect to the left edge of its column."
+#	$self->out( \"{\\pard\\trowd\\trgaph10\\trleft10");
+	$self->out( \"{\\trowd\\trgaph10\\trleft10");
+
 	my $current_cell_end = 0;
 	foreach my $cell (@cells){
 		my $width = $cell->attr('width');
-		if($cell_count == 1){
-			$width = 100;			
-		}
-		my $base_cell_width = $twips_width / $cell_count;		
-		if($width){
-			$width =~ s/\%//; 
-			$base_cell_width = $twips_width * $width / 100;			
-		}
+#		if($cell_count == 1){
+#			$width = 100;
+#		}
+		my $base_cell_width = $twips_width / $cell_count;
+		if($width =~ m/.+/){
+		  if($width =~ m/\%/){
+			$width =~ s/\%//;
+			$base_cell_width = $twips_width * $width / 100;
+		      }
+		  else{
+		    $width =~ s/(?:\.00)px//; # remove irrelvant part of 240.00px
+		    my $factor = ($width / $PAGE_PX_WIDTH);
+#		    print "factor: $factor\n";
+		    $base_cell_width = $twips_width * $factor;
 
+		  }
+		}
+#		print "width: $width // $base_cell_width\n";
 		$current_cell_end = $current_cell_end + int ($base_cell_width) + $cell_padding;
 		$self->out( \"\\cellx$current_cell_end" );
 	}
@@ -99,13 +116,15 @@ sub tr_start{
 
 sub tr_end{
 	my $self = shift;
-	$self->out( \"\\row}\n");
+	$self->out( \"\\row\\pard\\par}\n");
 }
 
 sub td_start{
 	my $self = shift;
 	my $node = shift;
 	$self->out( \'\pard\intbl' );
+#	$self->out( \'\pard\par\intbl' );
+	$self->{'insideTable'} = 1;
 	my $alignment = $node->attr('align');
 	if($alignment){
 		# use if-else, because it seems like the webserver is still on perl 5.8
@@ -113,54 +132,57 @@ sub td_start{
 			$self->out(\'\ql');
 		}
 		elsif($alignment eq 'right'){
-			$self->out(\'\qr');			
+			$self->out(\'\qr');
 		}
 		elsif($alignment eq 'center'){
-			$self->out(\'\qc');			
-		}		
-		elsif($alignment eq 'justify'){
-			$self->out(\'\qj');			
+			$self->out(\'\qc');
 		}
-		# html has attribute value 'char', rtf has possible align "qd (distributed)" 
-		# both do not have an obvious match to the other side. 						
+		elsif($alignment eq 'justify'){
+			$self->out(\'\qj');
+		}
+		# html has attribute value 'char', rtf has possible align "qd (distributed)"
+		# both do not have an obvious match to the other side.
 	}
-	
-	$self->out(' ');		
+
+	$self->out(' ');
 }
 
 sub td_end{
 	my $self = shift;
+	$self->{'insideTable'} = 0;
 	$self->out( \" \\cell\n");
 }
 
 sub th_start{
 	my $self = shift;
-	$self->out( \'\pard\intbl \b ' );	
+	$self->{'insideTable'} = 1;
+	$self->out( \'\pard\intbl \b ' );
 }
 
 sub th_end{
 	my $self = shift;
+	$self->{'insideTable'} = 0;
 	$self->out( \" \\cell\n");
 }
 
 # param is style string like 'font-size: 16pt; color:black;'
 # return font size in pt
 sub determine_font_size{
-	my $style = shift;	
+	my $style = shift;
 #	use CSS;
 #	$css->read_string($style); # does not work.
 #	my $css = CSS->new();
 #	print STDERR $css->output();
-	
+
 	# currently, we expect font-size to be in pt and not in px etc.
-	my ($font_size) = $style =~ m{font-size:\s*(\d+)\s*pt};	 		
+	my ($font_size) = $style =~ m{font-size:\s*(\d+)\s*pt};
 	return $font_size;
 }
 
 # param is style string like 'font-size: 16pt;font-family: Arial;'
 # return font name if it's contained in font_map.
 sub determine_font{
-	my $style = shift;	
+	my $style = shift;
 
 	my @fonts = $style =~ m{font-family:
 		\s*
@@ -171,7 +193,7 @@ sub determine_font{
 		,?
 		)+;
 	}gx;
-	@fonts = map {lc($_)} @fonts; 
+	@fonts = map {lc($_)} @fonts;
 	# font-family: verdana, sans-serif;
 	# font-family: 'Times New Roman',Times,serif
 	my $font_number;
@@ -181,7 +203,7 @@ sub determine_font{
 			last;
 		}
 	}
-	
+
 	return $font_number;
 }
 
@@ -201,12 +223,12 @@ sub add_font{
 sub start_style_check{
 	my $self = shift;
 	my $node = shift;
-	
+
 	my $style = $node->attr('style');
 	if( $style ){
-		my $font_number = determine_font($style); 
+		my $font_number = determine_font($style);
 		if($font_number){
-			$self->add_font($font_number);			
+			$self->add_font($font_number);
 		}
 		my $font_size = determine_font_size($style);
 		if($font_size){
@@ -217,20 +239,20 @@ sub start_style_check{
 		}
 	}
 	$self->out(\'');
-	
+
 }
 
 sub end_style_check{
 	my $self = shift;
 	my $node = shift;
-	
+
 	my $style = $node->attr('style');
 	if($style){
-		my $font_number = determine_font($style); 
+		my $font_number = determine_font($style);
 		if($font_number){
-			$self->out(\"}\n");			
-		}		
-		my $font_size =determine_font_size($style); 
+			$self->out(\"}\n");
+		}
+		my $font_size =determine_font_size($style);
 		if($font_size){
 			# close font size block.
 			$self->out(\"}\n");
@@ -242,13 +264,13 @@ sub end_style_check{
 sub div_start{
 	my $self = shift;
 	my $node = shift;
-	$self->start_style_check($node);	
+	$self->start_style_check($node);
 }
 
 sub div_end{
 	my $self = shift;
 	my $node = shift;
-	$self->end_style_check($node);	
+	$self->end_style_check($node);
 }
 
 # overridden from Format::RTF
@@ -274,18 +296,98 @@ sub font_table {
 
 END
 
-        map {
-        ;                       # custom-dumb escaper:
-        my $x = $_;
-        $x =~ s/([\x00-\x1F\\\{\}\x7F-\xFF])/sprintf("\\'%02x", $1)/g;
-        $x =~ s/([^\x00-\xFF])/'\\uc1\\u'.((ord($1)<32768)?ord($1):(ord($1)-65536)).'?'/eg;
-        $x;
-        }
-        $self->{'fontname_body'}     || 'Times',
-        $self->{'fontname_code'}     || 'Courier New',
-        $self->{'fontname_headings'} || 'Arial',
-        ;
+	map {
+	;                       # custom-dumb escaper:
+	my $x = $_;
+	$x =~ s/([\x00-\x1F\\\{\}\x7F-\xFF])/sprintf("\\'%02x", $1)/g;
+	$x =~ s/([^\x00-\xFF])/'\\uc1\\u'.((ord($1)<32768)?ord($1):(ord($1)-65536)).'?'/eg;
+	$x;
+	}
+	$self->{'fontname_body'}     || 'Times',
+	$self->{'fontname_code'}     || 'Courier New',
+	$self->{'fontname_headings'} || 'Arial',
+	;
 }
+
+
+# ------------------------------------------------------------------------
+sub emit_para {    # rather like showline in FormatPS
+    my $self = shift;
+
+    my $para = $self->{'Para'};
+    $self->{'Para'} = undef;
+
+    #### emit_para called by: (caller(1) )[3];
+
+    unless ( defined $para ) {
+        #### emit_para with empty buffer...
+        return;
+    }
+    
+    $para =~ s/^ +//s;
+    $para =~ s/ +$//s;
+
+    # And now: a not terribly clever algorithm for inserting newlines
+    # at a guaranteed harmless place: after a block of whitespace
+    # after the 65th column.  This was copied from RTF::Writer.
+    $para =~ s/(
+       [^\cm\cj\n]{65}        # Snare 65 characters from a line
+       [^\cm\cj\n\x20]{0,50}  #  and finish any current word
+      )
+      (\x20{1,10})(?![\cm\cj\n]) # capture some spaces not at line-end
+     /$1$2\n/gx    # and put a NL before those spaces
+        ;
+
+    # if the paragraph is in a table, we need \intbl
+    my $intbl = "";
+
+# if emit_para had the node param, we could use this:
+#    my $lineage = join(" ", $node->lineage_tag_names());
+#    if($lineage =~ m/td|th/){
+#     # $self->out(\'\\intbl');
+#      $intbl = "\\intbl";
+#    }
+# because to get $node through several layers of subroutine calls, 
+# I would have to rewrite the parent module.
+    if($self->{'insideTable'}){
+      $intbl = "\\intbl";
+    }
+
+    #print "para: $para\n";
+    $self->collect(
+        sprintf(
+            '{\pard\sa%d\li%d\ri%d%s\plain' . "\n",
+#            '\pard\sa%d\li%d\ri%d%s\plain' . "\n",
+            #100 +
+            10 * $self->{'normal_halfpoint_size'} * ( $self->{'vspace'} || 0 ),
+
+            $self->{'lm'},
+            $self->{'rm'},
+
+            $self->{'center'} ? '\qc' : '\ql',
+        ),
+
+        defined( $self->{'next_bullet'} )
+        ? do {
+            my $bullet = $self->{'next_bullet'};
+            $self->{'next_bullet'} = undef;
+            sprintf "\\fi-%d\n%s",
+                4.5 * $self->{'normal_halfpoint_size'},
+                ( $bullet eq '*' ) ? "\\'95 " : ( rtf_esc($bullet) . ". " );
+            }
+        : (),
+
+        $para,
+        "\n\\par$intbl}\n\n",
+#        "\n\\par$intbl\n\n",
+    );
+
+    $self->{'vspace'} = undef;    # we finally get to clear it here!
+
+    return;
+}
+
+
 
 1;
 
@@ -328,7 +430,7 @@ L<HTML::Formatter>, L<HTML::FormatRTF>
 
 =head1 AUTHORS
 
-Ingo Wiarda <ingo_wiarda@dewarim.de> 
+Ingo Wiarda <ingo_wiarda@dewarim.de>
 
 =head1 COPYRIGHT AND LICENSE
 
--- HtmlToRtf.pl
+++ HtmlToRtf.pl
@@ -6,7 +6,27 @@ use File::Slurp;
 use HTML::FormatRTFx;
 
 
-my $utf_text = read_file( 'test/test.html', binmode => ':utf8' ) ;
-print HTML::FormatRTFx->format_string($utf_text);
+if(@ARGV < 2){
+    print "Usage: perl HtmlToRtf.pl inputFile outputFile\n";
+    exit;
+}
 
+my $inputFile = $ARGV[0];
+my $outputFile = $ARGV[1];
+
+my $utf_text = read_file( $inputFile, binmode => ':utf8' ) ;
+
+open(my $fh, '>', $outputFile) or die("Could not open file $outputFile for output: $!");
+
+# remove extra whitespace, otherwise HTML::FormatRTF will try to insert it as verbatim whitespace:
+$utf_text =~ s/\s{2,}/ /gm;
+#print $utf_text;
+my $result = HTML::FormatRTFx->format_string($utf_text);
+
+print $fh $result;
+
+close($fh) or die("Could not close file $outputFile. $!");
+print $result;
+#my $formatter = HTML::FormatRTFx->new;
+#$formatter->format_string($utf_text);
 
